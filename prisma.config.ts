7b202513729883eba27b16736899f154338137dd
vaibhavdekatey <vaibhav2001dekatey@gmail.com>
2026-07-04 17:01:41 +0530

    typefix for prisma, added postinstall

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from '@prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
     url: process.env.DATABASE_URL,
   }
